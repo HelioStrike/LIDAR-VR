b9562b89040749d601f8335ea898f7987f7e5642
HelioStrike <rageofepicfury@gmail.com>
2018-12-19 21:05:01 +0530

    committed

--- algos/main.py
+++ algos/main.py
@@ -1,20 +1,18 @@
 import bpy
 import laspy
 
-las_file = laspy.file.File("/home/krypt/Downloads/AK_BrooksCamp_2012_000001/AK_BrooksCamp_2012_000001.las", mode="r")
-
+las_file = laspy.file.File("/home/krypt/Downloads/AK_BrooksCamp_2012_000015/AK_BrooksCamp_2012_000015.las", mode="r")
 
 # mesh arrays
 verts = []  # the vertex array
 faces = []  # the face array
  
 # scaling factors
-scaleX = 1e-7
-scaleY = 1e-8
-scaleZ = 1e-3
+scaleX, scaleY, scaleZ = las_file.header.scale
+offsetX, offsetY, offsetZ = las_file.header.offset
 
-for i in range(int(len(las_file.X)/100)):
-    verts.append((las_file.X[i]*scaleX, las_file.Y[i]*scaleY, las_file.Z[i]*scaleZ))
+for i in range(len(las_file.X)):
+    verts.append((las_file.X[i]*scaleX + offsetX, las_file.Y[i]*scaleY + offsetY, las_file.Z[i]*scaleZ + offsetZ))
 
 #create mesh and object
 mesh = bpy.data.meshes.new("wave")
@@ -26,4 +24,3 @@ bpy.context.scene.objects.link(object)
  
 #create mesh from python data
 mesh.from_pydata(verts,[],faces)
-mesh.update(calc_edges=True)
