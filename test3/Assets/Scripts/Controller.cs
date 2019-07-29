b0104c179f30eefb1ae68a0ab8197bd4124662fd
HelioStrike <rageofepicfury@gmail.com>
2019-07-29 22:00:29 +0530

    committed

---
+++ test3/Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	float rotationY = 0F;
+	public float speed = 1;
+
+	void Update ()
+	{
+		
+		//mouse input code
+		float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+		
+		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+		rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+		
+		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+	
+		//movement code
+		this.transform.Translate(new Vector3(speed*Input.GetAxis("Horizontal"), (Input.GetKey("space")? 1:0)*speed, speed*Input.GetAxis("Vertical")));
+
+	}
+	
+}
---
+++ test3/Assets/Scripts/PointCloudRenderer.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCloudRenderer : MonoBehaviour {
+
+	public string path;
+	public int numX, numY, numZ;
+	public Material mat;
+	public Shader shader;
+
+	// Use this for initialization
+	void Start () {
+		string[] lines = File.ReadAllLines(path);
+		
+		int[,,] point_bins = new int[numX+1,numY+1,numZ+1];
+		for(int i = 0; i < numX; i++)
+		{
+			for(int j = 0; j < numY; j++)
+			{
+				for(int k = 0; k < numZ; k++)
+				{
+					point_bins[i,j,k] = 0;
+				}
+			}
+		}
+
+		int inf = 99999999;
+		float currX,currY,currZ,minX=inf,minY=inf,minZ=inf,maxX=-inf,maxY=-inf,maxZ=-inf;
+		int totPoints = int.Parse(lines[1].Split(' ')[0]);
+		for(int i = 2; i < totPoints+2; i++)
+		{
+			string[] divs = lines[i].Split(' ');
+			
+			currX = float.Parse(divs[0]);
+			currY = float.Parse(divs[1]);
+			currZ = float.Parse(divs[2]);
+
+			if(currX < minX) minX = currX;
+			if(currX > maxX) maxX = currX;
+
+			if(currY < minY) minY = currY;
+			if(currY > maxY) maxY = currY;
+
+			if(currZ < minZ) minZ = currZ;
+			if(currZ > maxZ) maxZ = currZ;
+		} 
+
+		float diffX = Math.Abs(minX - maxX);
+		float diffY = Math.Abs(minY - maxY);
+		float diffZ = Math.Abs(minZ - maxZ);
+
+		int numPoints = 0;
+
+		for(int i = 2; i < totPoints+2; i++)
+		{
+			string[] divs = lines[i].Split(' ');
+			
+			currX = numX*(float.Parse(divs[0]) - minX)/diffX;
+			currY = numY*(float.Parse(divs[1]) - minY)/diffY;
+			currZ = numZ*(float.Parse(divs[2]) - minZ)/diffZ;
+
+			if(point_bins[(int)currX,(int)currY,(int)currZ] == 0) { numPoints++;}
+			
+			point_bins[(int)currX,(int)currY,(int)currZ] = 1;
+		} 
+
+		int curr = 0;
+		Vector3[] points = new Vector3[numPoints];
+		
+		for(int i = 0; i < numX; i++)
+		{
+			for(int j = 0; j < numY; j++)
+			{
+				for(int k = 0; k < numZ; k++)
+				{
+					if(point_bins[i,j,k] == 1)
+					{
+						points[curr] = new Vector3(i*diffX/diffZ, j*diffY/diffZ, k);
+						curr++;
+					}
+				}
+			}
+		}
+
+		Vector2[] uvs = new Vector2[points.Length];
+		Color[] colors = new Color[points.Length];
+		for(int i = 0; i < points.Length; i++)
+		{
+			uvs[i] = new Vector2(points[i].x, points[i].z);
+			colors[i] = Color.red;
+		}
+
+		gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+ 		mat.shader = shader;
+		this.GetComponent<MeshRenderer>().material = mat;
+	    Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+
+		Debug.Log(points.Length);
+
+		mesh.vertices = points;
+		mesh.uv = uvs;
+		mesh.colors = colors;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}	
+}
